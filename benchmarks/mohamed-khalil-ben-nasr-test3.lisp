ae59ce2919881bf71e79f66f281dafa12ccd99e9
Mohamed Khalil Ben Nasr <mohamedkhalilbennasr@mac.mynetworksettings.com>
2025-12-05 15:29:01 -0500

    made correction to tests

--- benchmarks/mohamed-khalil-ben-nasr-test3.lisp
+++ benchmarks/mohamed-khalil-ben-nasr-test3.lisp
@@ -1,10 +1,9 @@
 (define (inc2 x) (+ x 2))
-(define (square x) (* x x))
-(define (f x) (square (inc2 x)))
+(define (twice x) (+ x x))
+(define (f x) (twice (inc2 x)))
 
 (print
   (+ (f 3)
      (+ (f (sub1 4))
-        (+ (square (inc2 (sub1 5)))
+        (+ (twice (inc2 (sub1 5)))
            (f (+ 1 1))))))
-
